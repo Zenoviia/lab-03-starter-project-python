a2ad98c0a5e3e9200a1bde2baecf5add721487a3
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2025-06-04 22:46:14 +0300

    Update api.py

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,20 @@
+import numpy as np
+
 from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get("")
 
-@router.get('')
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    return {"msg": "Hello, World!"}
+
+@router.get("/matmul")
+
+def matmul():
+    a = np.random.rand(10, 10)
+    b = np.random.rand(10, 10)
+    
+    product = np.dot(a, b)
+    
+    return {"matrix_a": a.tolist(), "matrix_b": b.tolist(), "product": product.tolist()}
