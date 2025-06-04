7f6fd557a97148757bca905f0d3e6a1c3932b11e
Zenoviia <144124130+Zenoviia@users.noreply.github.com>
2025-06-04 22:41:05 +0300

    Update app.py

--- spaceship/app.py
+++ spaceship/app.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import FileResponse
-
 from spaceship.config import Settings
 from spaceship.routers import api, health
+from starlette.responses import FileResponse
 
 
 def make_app(settings: Settings) -> FastAPI:
@@ -15,14 +14,16 @@ def make_app(settings: Settings) -> FastAPI:
     )
     app.state.settings = settings
 
+    print("Tkachenko Zenoviia")
+
     if settings.debug:
-        app.mount('/static', StaticFiles(directory='build'), name='static')
+        app.mount("/static", StaticFiles(directory="build"), name="static")
 
-    app.include_router(api.router, prefix='/api', tags=['api'])
-    app.include_router(health.router, prefix='/health', tags=['health'])
+    app.include_router(api.router, prefix="/api", tags=["api"])
+    app.include_router(health.router, prefix="/health", tags=["health"])
 
-    @app.get('/', include_in_schema=False, response_class=FileResponse)
+    @app.get("/", include_in_schema=False, response_class=FileResponse)
     async def root() -> str:
-        return 'build/index.html'
+        return "build/index.html"
 
     return app
